2fdacc0a57f1277951c7a390e408c0c399e837c0
VictorD <flame.el22@gmail.com>
2018-06-10 16:47:04 -0300

    Fix en el bootstrap
    Se hizo una correcion en el bootstrap el cual no inicializaba correctamente las bases de datos, impidiendo que algunos metodos del api no funcionacen correctamente.

--- grails-app/init/iisoft/acdp/backend/BootStrap.groovy
+++ grails-app/init/iisoft/acdp/backend/BootStrap.groovy
@@ -11,44 +11,73 @@ class BootStrap {
         category1.save()
         category2.save()
 
-        def publication1 = new Publication(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idCategory: category1.id,name: "Retrospective",
-                title: "RetrospectiveIISoft",
-                whoPublishedIt: "Diego",
-                date: new Date(2018, 06, 22))
-        def publication2 = new Publication(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idCategory: category2.id,name: "BDD",
-                title: "BDDIISoft",
-                whoPublishedIt: "Pablo",
-                date: new Date(2017, 05, 10))
-        def publication3 = new Publication(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idCategory: category2.id,name: "TDD",
-                title: "TDDIISoft",
-                whoPublishedIt: "Diego",
-                date: new Date(2016, 02, 14))
+        def publication1 = new Publication  (   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                idCategory      : category1.id,
+                                                name            : "Retrospective",
+                                                title           : "RetrospectiveIISoft",
+                                                whoPublishedIt  : "Diego",
+                                                date            : new Date(2018, 06, 22)
+                                            )
+
+        def publication2 = new Publication  (   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                idCategory      : category2.id,
+                                                name            : "BDD",
+                                                title           : "BDDIISoft",
+                                                whoPublishedIt  : "Pablo",
+                                                date            : new Date(2017, 05, 10)
+                                            )
+
+        def publication3 = new Publication  (   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                idCategory      : category2.id,
+                                                name            : "TDD",
+                                                title           : "TDDIISoft",
+                                                whoPublishedIt  : "Diego",
+                                                date            : new Date(2016, 02, 14)
+                                            )
 
         category1.publications = [publication1]
         category2.publications = [publication2, publication3]
-        category1.save()
-        category2.save()
+//        category1.save()
+//        category2.save()
+        publication1.save()
+        publication2.save()
+        publication3.save()
 
-        Commentary commentaryForPub1        = new Commentary(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idPublication: publication1.id,
-                whoPublishedIt: "Diego",
-                date: new Date(2013, 07, 22))
-        Commentary otherCommentaryForPub1        = new Commentary(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idPublication: publication1.id,
-                whoPublishedIt: "Diego",
-                date: new Date(2013, 07, 22))
+        Commentary commentaryForPub1        = new Commentary(   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                                idPublication   : publication1.id,
+                                                                whoPublishedIt  : "Diego",
+                                                                date            : new Date(2013, 07, 22)
+                                                            )
 
-        Commentary commentaryForPub2        = new Commentary(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idPublication: publication2.id,
-                whoPublishedIt: "Diego",
-                date: new Date(2013, 07, 22))
+        Commentary otherCommentaryForPub1   = new Commentary(   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                                idPublication   : publication1.id,
+                                                                whoPublishedIt  : "Diego",
+                                                                date            : new Date(2013, 07, 22)
+                                                            )
 
-        Commentary commentaryForPub3        = new Commentary(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idPublication: publication3.id,
-                whoPublishedIt: "Diego",
-                date: new Date(2013, 07, 22))
-        Commentary otherCommentaryForPub3        = new Commentary(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idPublication: publication3.id,
-                whoPublishedIt: "Diego",
-                date: new Date(2013, 07, 22))
-        Commentary anotherCommentaryForPub3        = new Commentary(text:"CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS" ,idPublication: publication3.id,
-                whoPublishedIt: "Diego",
-                date: new Date(2013, 07, 22))
+        Commentary commentaryForPub2        = new Commentary(   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                                idPublication   : publication2.id,
+                                                                whoPublishedIt  : "Diego",
+                                                                date            : new Date(2013, 07, 22)
+                                                            )
+
+        Commentary commentaryForPub3        = new Commentary(   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                                idPublication   : publication3.id,
+                                                                whoPublishedIt  : "Diego",
+                                                                date            : new Date(2013, 07, 22)
+                                                            )
+
+        Commentary otherCommentaryForPub3   = new Commentary(   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                                idPublication   : publication3.id,
+                                                                whoPublishedIt  : "Diego",
+                                                                date            : new Date(2013, 07, 22)
+                                                            )
+
+        Commentary anotherCommentaryForPub3 = new Commentary(   text            : "CHAMULLA ACA IVAN MANDAME EL STRING QUE QUIERAS",
+                                                                idPublication   : publication3.id,
+                                                                whoPublishedIt  : "Diego",
+                                                                date            : new Date(2013, 07, 22)
+                                                            )
 
         commentaryForPub1.save()
         otherCommentaryForPub1.save()
